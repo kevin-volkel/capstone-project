5dd09f88b992f98b66ed122a500a92d07bd28265
Kevin <kevinator900@gmail.com>
2021-05-10 13:03:47 -0700

    added other pages

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,13 +1,15 @@
 import Daily from "./Daily";
+import Hourly from './Hourly'
+import Weekly from './Weekly'
 import Slider from './Slider'
  
 const Background = ({ active, data }) => {
   return (
     <>
       <div className="background">
-        {(active === "Daily") && <><Daily/><Slider objects = {data}/></>}
-        {/* {(active === "Hourly") && <><Hourly/><Slider objects = {data}/>} */}
-        {/* {(active === "Weekly") && <><Weekly/><Slider objects = {data}/>} */}
+        {(active === "Daily") && <><Daily/><Slider objects = {data.daily}/></>}
+        {(active === "Hourly") && <><Hourly/><Slider objects = {data.hourly}/></>}
+        {(active === "Weekly") && <><Weekly/><Slider objects = {data.weekly}/></>}
       </div>
     </>
   );
